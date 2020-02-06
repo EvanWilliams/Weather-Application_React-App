97139e333ee78d76e9cba0775ece792021c248b0
Evan Williams <edw@evanwilliams.info>
2020-02-05 17:29:07 -0800

    implement useState hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,222 @@
-import React from 'react';
-import './components/WeatherDisplay.js'
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import WeatherDisplay from './components/WeatherDisplay.js'
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Hello! </h1>
-    </div>
 
+  const [currentWeatherData, setcurrentWeatherData] = useState({
+    "latitude":42.3601,
+    "longitude":-71.0589,
+    "timezone":"America/New_York",
+    "currently":{
+    "summary":"Drizzle",
+    "icon":"rain",
+    "precipProbability":0.9,
+    "precipType":"rain",
+    "temperature":66.1,
+   
+    "humidity":0.83,
+    "pressure":1010.34,
+    "windSpeed":5.59
+    },
+    "minutely":{
+    "summary":"Light rain stopping in 13 min., starting again 30 min. later.",
+    "icon":"rain",
+    },
+    "hourly":{
+    "summary":"Rain starting later this afternoon, continuing until this evening.",
+    "icon":"rain",
+    "data":[
+          {
+            "time":1509991200,
+            "summary":"Mostly Cloudy",
+            "icon":"partly-cloudy-day",
+            "precipType":"rain",
+            "temperature":65.76,
+            "pressure":1010.57,
+            "windSpeed":4.23,
+          }
+        ]
+      },
+      "daily":{
+      "summary":"Mixed precipitation throughout the week, with temperatures falling to 39°F on Saturday.",
+      "icon":"rain",
+      "data":[
+          {
+          "time":1509944400,
+          "summary":"Rain starting in the afternoon, continuing until evening.",
+          "pressure":1012.93,
+          "windSpeed":3.22,
+          }
+      ]
+    }
+  });
+  const [customWeatherData, setcustomWeatherData] = useState(
+    {
+      "latitude":42.3601,
+      "longitude":-71.0589,
+      "timezone":"America/New_York",
+      "currently":{
+      "summary":"Drizzle",
+      "icon":"rain",
+      "precipProbability":0.9,
+      "precipType":"rain",
+      "temperature":66.1,
+     
+      "humidity":0.83,
+      "pressure":1010.34,
+      "windSpeed":5.59
+      },
+      "minutely":{
+      "summary":"Light rain stopping in 13 min., starting again 30 min. later.",
+      "icon":"rain",
+      },
+      "hourly":{
+      "summary":"Rain starting later this afternoon, continuing until this evening.",
+      "icon":"rain",
+      "data":[
+            {
+              "time":1509991200,
+              "summary":"Mostly Cloudy",
+              "icon":"partly-cloudy-day",
+              "precipType":"rain",
+              "temperature":65.76,
+              "pressure":1010.57,
+              "windSpeed":4.23,
+            }
+          ]
+        },
+        "daily":{
+        "summary":"Mixed precipitation throughout the week, with temperatures falling to 39°F on Saturday.",
+        "icon":"rain",
+        "data":[
+            {
+            "time":1509944400,
+            "summary":"Rain starting in the afternoon, continuing until evening.",
+            "pressure":1012.93,
+            "windSpeed":3.22,
+            }
+        ]
+      }
+    }
   );
+  const [currentLocation, setcurrentLocation] = useState([{"lat":"82.3", "long":"-147.1"}]);
+  const [customLocation, setcustomLocation] = useState([{"lat":null, "long":null}]);
+  var currentLocationTimer,customLocationTimer = null;
+
+  useEffect(() => {
+    showPosition();
+  },[]);
+
+  const showPosition = async() => {
+    if(navigator.geolocation) {
+        await navigator.geolocation.getCurrentPosition((position) => {
+            setcurrentLocation({"lat":position.coords.latitude.toString(), "long":position.coords.longitude.toString()});
+        });
+        setPolling();
+    } else {
+        alert("Sorry, your browser does not support HTML5 geolocation.");
+    }
+  }
+  function setPolling (lat,long) {
+    //set the app to poll for changes in the weather.
+    if(currentLocationTimer){
+        clearInterval(currentLocationTimer);
+        currentLocationTimer = null;
+      }
+      callDarksky(currentLocation[0].lat,currentLocation[0].long,true);
+      currentLocationTimer = setInterval( () => callDarksky(currentLocation[0].lat,currentLocation[0].long,true), 30000);
+
+    if(customLocation[0].lat != undefined && customLocation[0].long != undefined){
+      if(customLocationTimer){
+        clearInterval(this.customLocationTimer);
+        customLocationTimer = null;
+      }
+      callDarksky(customLocation[0].lat,customLocation[0].long, false);
+      customLocationTimer = setInterval( () => callDarksky(customLocation[0].lat,customLocation[0].long, false), 30000);
+    }
+
+  }
+
+  const callDarksky = async (lat,long,isCurrent) => {
+    let proxy = `https://cors-anywhere.herokuapp.com/`;
+    let endpoint = `https://api.darksky.net/forecast/fa37bc51f1954bd226b9cdf3ad2c6f7e/${lat},${long}`;
+
+    const response = await fetch(proxy + endpoint)
+    .then(res => res.json())
+    .then( data => { 
+      debugger;
+
+      if(data.currently.icon){
+        data.currently.icon = data.currently.icon.toUpperCase().replace(/-/g, '_');
+      }
+      if(data.hourly.icon){
+        data.hourly.icon = data.hourly.icon.toUpperCase().replace(/-/g, '_');
+      }
+      if(data.daily.icon){
+        data.daily.icon  = data.daily.icon.toUpperCase().replace(/-/g, '_');
+      }
+      data.heat = Math.ceil((32 - data.currently.temperature)*3);
+
+      if(isCurrent && data){
+        setcurrentWeatherData(data);
+        console.log(currentWeatherData);
+        console.log(data);
+      }
+      else if (data){
+        setcustomWeatherData(data);
+        console.log(data);
+      }
+
+      if(isCurrent){
+        document.getElementById('weather-display').style.filter= "hue-rotate("+data.heat+"deg)";
+      }
+    });
+      //     if(this.state.isCurrent){
+      //       document.getElementById('weather-display').style.filter= "hue-rotate("+this.state.heat+"deg)";
+      //     }
+      //     else{
+      //       document.getElementById('weather-display_configurable').style.filter= "hue-rotate("+this.state.heat+"deg)";
+      //       document.getElementById('latinput').value  = lat;
+      //       document.getElementById('longinput').value = long;
+      //     }
+  }
+  return(
+      <div id="App">
+        <WeatherDisplay 
+          isCurrent={true}
+          latitude={currentWeatherData.latitude}
+          longitude={currentWeatherData.longitude}
+          timezone={currentWeatherData.timezone}
+          summary={currentWeatherData.currently.summary}
+          currentIcon={currentWeatherData.currently.icon}
+          currentTemperature={currentWeatherData.currently.temperature}
+          currentWindspeed={currentWeatherData.currently.windSpeed}
+          currentPressure={currentWeatherData.currently.pressure}
+          hourlySummary={currentWeatherData.hourly.summary}
+          hourlyIcon={currentWeatherData.hourly.icon}
+          dailySummary={currentWeatherData.daily.summary}
+          dailyIcon={currentWeatherData.daily.icon}
+        />
+        { true ? (
+        <WeatherDisplay 
+          isCurrent={false}
+          latitude={customWeatherData.latitude}
+          longitude={customWeatherData.longitude}
+          timezone={customWeatherData.timezone}
+          summary={customWeatherData.currently.summary}
+          currentIcon={customWeatherData.currently.icon}
+          currentTemperature={customWeatherData.currently.temperature}
+          currentPressure={customWeatherData.currently.pressure}
+          hourlySummary={customWeatherData.hourly.summary}
+          hourlyIcon={customWeatherData.hourly.icon}
+          dailySummary={customWeatherData.daily.summary}
+          dailyIcon={customWeatherData.daily.icon}
+        /> )
+          : (<div/>)
+        } }
+      </div>
+    );
 }
 
 export default App;
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,147 +2,102 @@ import React from 'react';
 import './WeatherDisplay.css'
 import Skycons from 'react-skycons'
 
-class WeatherDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: [],
-      isCurrent: this.props.iscurrent
-    };
+const WeatherDisplay = ({isCurrent, latitude,longitude,timezone,summary,currentIcon,currentTemperature,currentPressure,currentWindspeed,hourlySummary,hourlyIcon,dailySummary,dailyIcon}) => {
+      
+  function handleChange(e) {
+    this.props.onInputChange(e.target.value);
   }
+
   
-  showPosition(self) {
-      if(navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-              self.setPolling(position.coords.latitude,position.coords.longitude);
-          });
-      } else {
-          alert("Sorry, your browser does not support HTML5 geolocation.");
+  if(isCurrent){
+        var controlsHTML = (
+      <div>
+        <div className="weather-description">
+          Weather data for your current location
+        </div>
+        <div className="location-info">
+          <div className="location-info--row">
+           <div className="location-info--title">Latitude&nbsp;:</div>
+           <div className="location-info--data">{latitude} </div>
+          </div>
+          <div className="location-info--row">
+            <div className="location-info--title">Longitude&nbsp;:</div>
+            <div className="location-info--data">{longitude} </div>
+          </div>
+          <div className="location-info--row"> 
+            <div className="location-info--title">Timezone&nbsp;:</div>
+            <div className="location-info--data"> {timezone} </div>
+          </div>
+        </div> 
+      </div>)
       }
-    }
-  componentDidMount() {
-    this.showPosition(this);
-  }
-  updateLocation(e){
-      let lat = parseInt(document.getElementById('latinput').value);
-      let long = parseInt(document.getElementById('longinput').value);
-
-      this.setPolling(lat,long);
-
-  }
-  setPolling(lat,long){
-    if(this.timer){
-        clearInterval(this.timer);
-        this.timer = null;
-      }
-      this.callDarksky(lat,long);
-      this.timer = setInterval(()=> this.callDarksky(lat,long), 30000);
-  }
-  callDarksky(lat,long){
-    let proxy = `https://cors-anywhere.herokuapp.com/`;
-    let endpoint = `https://api.darksky.net/forecast/fa37bc51f1954bd226b9cdf3ad2c6f7e/${lat},${long}`;
-
-    fetch(proxy+endpoint)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result,
-            currentIcon: result.currently.icon.toUpperCase().replace(/-/g, '_'),
-            hourlyIcon: result.hourly.icon.toUpperCase().replace(/-/g, '_'),
-            dailyIcon : result.daily.icon.toUpperCase().replace(/-/g, '_'),
-            heat: Math.ceil((32 - result.currently.temperature)*3),
-            latitude:0,
-            longitude:0
-          });
-          if(this.state.isCurrent){
-            document.getElementById('weather-display').style.filter= "hue-rotate("+this.state.heat+"deg)";
-          }
-          else{
-            document.getElementById('weather-display_configurable').style.filter= "hue-rotate("+this.state.heat+"deg)";
-            document.getElementById('latinput').value  = lat;
-            document.getElementById('longinput').value = long;
-          }
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-  render() {
-    const { error, isLoaded, items, isCurrent} = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else if(isCurrent){
-
+      else{(
+        controlsHTML = <div id="weather-display_configurable">
+        <div className="weather-description">
+          <div className="button-disclaimer">Enter in your own Lat and Long</div>
+          <div className="button"><button>Go</button></div>
+        </div>
+        <div className="location-info">
+          <div className="location-info--row">
+           <div className="location-info--title">Latitude&nbsp;:</div>
+           <div className="location-info--data">
+              <input type="text" placeholder="" id="latinput"/>
+           </div>
+          </div>
+          <div className="location-info--row">
+            <div className="location-info--title">Longitude&nbsp;:</div>
+            <div className="location-info--data">
+              <input type="text" placeholder="" id="longinput"/>
+            </div>
+          </div>
+          <div className="location-info--row"> 
+            <div className="location-info--title">Timezone&nbsp;:</div>
+            <div className="location-info--data"> {timezone} </div>
+          </div>
+        </div>
+      </div>
+      )}
       return (
         <div id="weather-display">
-          <div class="weather-description">
-            Weather data for your current location
-          </div>
-          <div class="location-info">
-            <div class="location-info--row">
-             <div class="location-info--title">Latitude&nbsp;:</div>
-             <div class="location-info--data">{items.latitude} </div>
-            </div>
-            <div class="location-info--row">
-              <div class="location-info--title">Longitude&nbsp;:</div>
-              <div class="location-info--data">{items.longitude} </div>
-            </div>
-            <div class="location-info--row"> 
-              <div class="location-info--title">Timezone&nbsp;:</div>
-              <div class="location-info--data"> {items.timezone} </div>
-            </div>
-          </div>
-          <div class="current-weather">
+        {controlsHTML}
+          <div className="current-weather">
             <h4>The Weather at this location is currently :</h4>
-            <h2>{items.currently.summary}</h2>
-            <div class="weather-card">
-              <div class="current-weather--skycons">
+            <h2>{summary}</h2>
+            <div className="weather-card">
+              <div className="current-weather--skycons">
               <Skycons 
                 color='black' 
-                icon={this.state.currentIcon} 
+                icon={currentIcon} 
                 autoplay={true}/>
               </div>
-              <div class="weather-card--info">
-                <div>Temperature (Degrees F) : {items.currently.temperature}</div>
-                <div>Presssure (Millibars) : {items.currently.pressure}</div>
-                <div>Wind Speed (MPH) : {items.currently.windSpeed}</div>
+              <div className="weather-card--info">
+                <div>Temperature (Degrees F) : {currentTemperature}</div>
+                <div>Presssure (Millibars) : {currentPressure}</div>
+                <div>Wind Speed (MPH) : {currentWindspeed}</div>
               </div>
             </div>
           </div>
-          <div class="future-forcast">
-            <div class="todays-weather">
+          <div className="future-forcast">
+            <div className="todays-weather">
               <div><strong>Today's Weather</strong></div>
-              <h3>{items.hourly.summary}</h3>
-              <div class="weather-card">
-                <div class="current-weather--skycons">
+              <h3>{hourlySummary}</h3>
+              <div className="weather-card">
+                <div className="current-weather--skycons">
                 <Skycons 
                   color='black' 
-                  icon={this.state.hourlyIcon} 
+                  icon={hourlyIcon} 
                   autoplay={true}/>
                 </div>
               </div>
             </div>
-            <div class="tomorrows-weather">
+            <div className="tomorrows-weather">
             <div><strong>Tomorrow's Weather</strong></div>
-              <h3>{items.daily.summary}</h3>
-              <div class="weather-card">
-                <div class="current-weather--skycons">
+              <h3>{dailySummary}</h3>
+              <div className="weather-card">
+                <div className="current-weather--skycons">
                 <Skycons 
                   color='black' 
-                  icon={this.state.dailyIcon} 
+                  icon={dailyIcon} 
                   autoplay={true}/>
                 </div>
               </div>
@@ -150,80 +105,6 @@ class WeatherDisplay extends React.Component {
           </div>
         </div>
       );
-
-    } else {
-      return (
-        <div id="weather-display_configurable">
-          <div class="weather-description">
-            <div class="button-disclaimer">Enter in your own Lat and Long</div>
-            <div class="button"><button onClick={(e) => this.updateLocation(e)}>Go</button></div>
-          </div>
-          <div class="location-info">
-            <div class="location-info--row">
-             <div class="location-info--title">Latitude&nbsp;:</div>
-             <div class="location-info--data">
-                <input type="text" placeholder="" id="latinput"/>
-             </div>
-            </div>
-            <div class="location-info--row">
-              <div class="location-info--title">Longitude&nbsp;:</div>
-              <div class="location-info--data">
-                <input type="text" placeholder="" id="longinput"/>
-              </div>
-            </div>
-            <div class="location-info--row"> 
-              <div class="location-info--title">Timezone&nbsp;:</div>
-              <div class="location-info--data"> {items.timezone} </div>
-            </div>
-          </div>
-          <div class="current-weather">
-            <h4>The Weather at this location is currently :</h4>
-            <h2>{items.currently.summary}</h2>
-            <div class="weather-card">
-              <div class="current-weather--skycons">
-              <Skycons 
-                color='black' 
-                icon={this.state.currentIcon} 
-                autoplay={true}/>
-              </div>
-              <div class="weather-card--info">
-                <div>Temperature (Degrees F) : {items.currently.temperature}</div>
-                <div>Presssure (Millibars) : {items.currently.pressure}</div>
-                <div>Wind Speed (MPH) : {items.currently.windSpeed}</div>
-              </div>
-            </div>
-          </div>
-          <div class="future-forcast">
-            <div class="todays-weather">
-              <div><strong>Today's Weather</strong></div>
-              <h3>{items.hourly.summary}</h3>
-              <div class="weather-card">
-                <div class="current-weather--skycons">
-                <Skycons 
-                  color='black' 
-                  icon={this.state.hourlyIcon} 
-                  autoplay={true}/>
-                </div>
-              </div>
-            </div>
-            <div class="tomorrows-weather">
-            <div><strong>Tomorrow's Weather</strong></div>
-              <h3>{items.daily.summary}</h3>
-              <div class="weather-card">
-                <div class="current-weather--skycons">
-                <Skycons 
-                  color='black' 
-                  icon={this.state.dailyIcon} 
-                  autoplay={true}/>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-      
-    }
-  }
 }
 
 export default WeatherDisplay;
